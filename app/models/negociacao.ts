11dc331d0d285dcd6ab66dd7096f113100ba656c
Gabrielle Castro <gabriellebcastro@gmail.com>
2023-02-25 16:17:50 -0300

    finalizando curso

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,27 +1,17 @@
 export class Negociacao {
-    #data;
-    #quantidade;
-    #valor;
 
-    constructor(data, quantidade, valor){
-        this.#data = data;
-        this.#quantidade = quantidade;
-        this.#valor = valor;
+    constructor(
+        private _data: Date, 
+        public readonly quantidade: number, 
+        public readonly valor: number
+    ){}
+
+    get volume(): number {
+        return this.quantidade * this.valor;
     }
 
-    get data() {
-        return this.#data;
-    }
-
-    get quantidade() {
-        return this.#quantidade;
-    }
-
-    get valor() {
-        return this.#valor;
-    }
-
-    get volume() {
-        return this.#quantidade * this.#valor;
+    get data(): Date {
+        const data = new Date(this._data.getTime());
+        return data;
     }
 }
